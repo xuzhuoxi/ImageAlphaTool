9eed9ca23c243f4bfb8ca0216dab4a8b9d7d4f67
xuzhuoxi <xuzhuoxi@gmail.com>
2022-02-12 23:12:48 +0800

    1.更新版本号为v1.0.1

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 CRTDIR=$(cd `dirname $0`; pwd)
 
-goxc -os="linux darwin windows freebsd openbsd" -arch="amd64 arm" -n=ImageClearer -pv=1.0.0 -wd=${CRTDIR}/../src -d=${CRTDIR}/./release -include=*.go,README*,LICENSE*
+goxc -os="linux darwin windows freebsd openbsd" -arch="amd64 arm" -n=ImageClearer -pv=1.0.1 -wd=${CRTDIR}/../src -d=${CRTDIR}/./release -include=*.go,README*,LICENSE*
