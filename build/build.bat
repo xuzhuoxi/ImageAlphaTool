cfc2f30653ad8238934c5d4a04b2beb30aadcadb
xuzhuoxi <xuzhuoxi@gmail.com>
2023-01-27 22:22:03 +0800

    1. 更新Windows下构造脚本，增加MacOS平台的交叉编译。

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
-goxc.exe -os="linux windows freebsd openbsd" -arch="amd64 arm" -n=ImageClearer -pv=1.0.1 -wd=../src -d=./release -include=*.go,README*,LICENSE*
+goxc.exe -os="linux darwin windows freebsd openbsd" -arch="amd64 arm" -n=ImageClearer -pv=1.0.1 -wd=../src -d=./release -include=*.go,README*,LICENSE*
 
 pause
